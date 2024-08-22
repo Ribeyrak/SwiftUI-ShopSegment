c66a19a5461e80314fa6884e2910bc5acb81b8f9
Evhen <35118152+Ribeyrak@users.noreply.github.com>
2024-08-21 20:13:17 -0400

    Initial Commit

---
+++ SwiftUI-ShopSegment/SwiftUI_ShopSegmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ShopSegmentApp.swift
+//  SwiftUI-ShopSegment
+//
+//  Created by Evhen Lukhtan on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ShopSegmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
